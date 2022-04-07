a9c8205fd236482414f33b10d8a5bfd56b7f4911
ryangle <hcg1228@foxmail.com>
2022-04-07 09:19:15 +0800

    add WindowSwitcher

---
+++ src/Fengsao/WindowSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace Fengsao;
+
+public static class WindowSwitcher
+{
+    public static void Switch<TClosed, TShow>() where TClosed : Window where TShow : Window, new()
+    {
+        Show<TShow>();
+        Closed<TClosed>();
+    }
+
+    public static void Show<T>(T? window = null) where T : Window, new()
+    {
+        
+        var shell = System.Windows.Application.Current.MainWindow = window ?? new T();
+        shell?.Show();
+    }
+
+    public static void Closed<T>() where T : Window
+    {
+        var shell = System.Windows.Application.Current.Windows.OfType<Window>().FirstOrDefault(window => window is T);
+        shell?.Close();
+    }
+}
